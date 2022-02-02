3ea2a3e3bf9296873f6b9d0237ff12c0a6528ef3
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-02-02 11:27:34 +0200

    fix: add fbq_await for events
    Signed-off-by: Gabriel Solomon <hello@gabrielsolomon.ro>

--- resources/views/events.php
+++ resources/views/events.php
@@ -12,10 +12,18 @@ $events = $events ?? [];
 <!-- Facebook Pixel Code Events -->
 <?php if (is_array($events) && count($events)) { ?>
     <script>
+        function fbq_await() {
+            if (typeof fbq === 'function') {
+                <?php foreach ($events as $event) { ?>
+                fbq('track', '<?= $event->getEventName(); ?>', <?= EventProperties::for($event); ?>);
+                <?php } ?>
+            } else {
+                setTimeout(fbq_await, 250);
+            }
+        }
+
         document.addEventListener("DOMContentLoaded", function () {
-            <?php foreach ($events as $event) { ?>
-            fbq('track', '<?= $event->getEventName(); ?>', <?= EventProperties::for($event); ?>);
-            <?php } ?>
+            fbq_await();
         });
     </script>
 <?php } ?>
